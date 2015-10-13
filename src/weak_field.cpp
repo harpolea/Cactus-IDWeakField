b22a93d1ca998e0a3eef7d90733596e68f2d4174
Ian Hawke <I.Hawke@soton.ac.uk>
2015-10-13 20:06:58 +0100

    Add the C++ tricks for the schedule.

--- src/weak_field.cpp
+++ src/weak_field.cpp
@@ -10,3 +10,18 @@
 #include "weak_field.h"
 
 // How do I c++???
+
+// Stolen from EinsteinInitialData/Meudon_Bin_NS/src/Bin_NS.cc
+
+extern "C"
+void IDWeakField_initialise (CCTK_ARGUMENTS)
+{
+  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
+
+  CCTK_INFO ("Setting up weak field initial data");
+
+  // Then cheat by calling your own C++ routine from here.
+
+  CCTK_INFO ("Done setting up weak field initial data");
+}
