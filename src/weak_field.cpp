11bfb1fe1040470acf0e9963b06f0c1b75c5d0b9
harpolea <ah1e14@soton.ac.uk>
2015-10-13 19:39:14 +0100

    Made some files

---
+++ src/weak_field.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+
+#include <cctk.h>
+#include <cctk_Arguments.h>
+#include <cctk_Parameters.h>
+
+#include "weak_field.h"
+
+// How do I c++???
