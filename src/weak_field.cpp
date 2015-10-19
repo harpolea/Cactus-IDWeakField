9ed5697f3f0ccb4ea2fe15608e4fb76c5c41344c
harpolea <ah1e14@soton.ac.uk>
2015-10-19 16:38:18 +0100

    Corrected some stuff, initialised parameters in .par file. Bubble and rt seem to work, kh does not?

--- src/weak_field.cpp
+++ src/weak_field.cpp
@@ -49,27 +49,19 @@ void IDWeakField_initialise (CCTK_ARGUMENTS)
     // Other quantities in terms of Cactus units
     CCTK_REAL const coord_unit = cactusL / 1.0e+3;         // from km
     CCTK_REAL const rho_unit   = cactusM / pow(cactusL,3); // from kg/m^3
-    CCTK_REAL const ener_unit  = pow(cactusL,2);           // from c^2
+    //CCTK_REAL const ener_unit  = pow(cactusL,2);           // from c^2
     CCTK_REAL const vel_unit   = cactusL / cactusT / c_light; // from c
 
     CCTK_INFO ("Setting up coordinates");
 
     int const npoints = cctk_lsh[0] * cctk_lsh[1] * cctk_lsh[2];
-    vector<double> xx(npoints), yy(npoints), zz(npoints);
-
-#pragma omp parallel for
-    for (int i=0; i<npoints; ++i) {
-        xx[i] = x[i] * coord_unit;
-        yy[i] = y[i] * coord_unit;
-        zz[i] = z[i] * coord_unit;
-    }
 
     // I hope that the coordinates have been ordered logically
     double xmin, xmax, zmin, zmax;
-    xmin = xx[0];
-    xmax = xx[npoints-1];
-    zmin = zz[0];
-    zmax = zz[npoints-1];
+    xmin = x[0];
+    xmax = x[npoints-1];
+    zmin = z[0];
+    zmax = z[npoints-1];
 
     try {
     CCTK_VInfo (CCTK_THORNSTRING, "mass [M_sun]:       %g", mass);
@@ -79,12 +71,19 @@ void IDWeakField_initialise (CCTK_ARGUMENTS)
     double zcntr = 0.5 * (zmax + zmin);
     double z_smooth = 0.04 * (zmax - zmin);
 
+    // convert stuff to Cactus units
+    double _rho0 = rho0 / rho_unit;
+    double _rho1 = rho1 / rho_unit;
+    double _rho2 = rho2 / rho_unit;
+    double _kh_u1 = kh_u1 / vel_unit;
+    double _kh_u2 = kh_u2 / vel_unit;
+
     CCTK_INFO ("Filling in Cactus grid points");
 
 #pragma omp parallel for
     for (int i=0; i<npoints; ++i) {
 
-      double rr = RR + sqrt(xx[i]*xx[i] + yy[i]*yy[i] + zz[i]*zz[i]);
+      double rr = RR + sqrt(x[i]*x[i] + y[i]*y[i] + z[i]*z[i]);
 
       alp[i] = sqrt(1.0 - 2.0 * mass / rr);
 
@@ -103,18 +102,18 @@ void IDWeakField_initialise (CCTK_ARGUMENTS)
       kyz[i] = 0.0;
       kzz[i] = 0.0;
 
-      double g = G_grav * mass / rr;
+      double g = mass / rr;
 
       // y velocity zero everywhere
       vel[i+  npoints] = 0.0;
 
       if (CCTK_EQUALS (initial_hydro, "bubble")) {
 
-          rho[i] = rho0 * exp(-g * yy[i] / (eos_gamma * RR * alp[i]*alp[i]));
+          rho[i] = _rho0 * exp(-g * y[i] / (eos_gamma * RR * alp[i]*alp[i]));
 
           eps[i] = pow(rho[i], eos_gamma - 1.0) / (eos_gamma - 1.0);
 
-          double r_coord = sqrt(pow(xx[i]-bubble_x_pert,2) + pow(zz[i]-bubble_z_pert,2));
+          double r_coord = sqrt(pow(x[i]-bubble_x_pert*(xmax-xmin),2) + pow(z[i]-bubble_z_pert*(zmax-zmin),2));
 
           if (r_coord <= bubble_r_pert)
           {
@@ -128,39 +127,38 @@ void IDWeakField_initialise (CCTK_ARGUMENTS)
 
       } else if (CCTK_EQUALS (initial_hydro, "kh")) {
 
-          if (zz[i] < zcntr) {
-              rho[i] = (rho1 - (rho2-rho1) * exp((zz[i]-zcntr)/(0.025*(xmax-xmin))))/ rho_unit;
+          if (z[i] < zcntr) {
+              rho[i] = _rho1 - (_rho2-_rho1) * exp((z[i]-zcntr)/(0.025*(xmax-xmin)));
 
               // u
-              vel[i] = (kh_u1 - (kh_u2-kh_u1) * exp((zz[i]-zcntr)/(0.025*(xmax-xmin))))/ rho_unit;
+              vel[i] = _kh_u1 - (_kh_u2-_kh_u1) * exp((z[i]-zcntr)/(0.025*(xmax-xmin)));
 
           } else {
-              rho[i] = (rho2 + (rho2-rho1) * exp((-zz[i]+zcntr)/(0.025*(xmax-xmin))))/ rho_unit;
+              rho[i] = _rho2 + (_rho2-_rho1) * exp((-z[i]+zcntr)/(0.025*(xmax-xmin)));
 
               // u
-              vel[i] = (kh_u2 + (kh_u2-kh_u1) * exp((-zz[i]+zcntr)/(0.025*(xmax-xmin))))/ rho_unit;
+              vel[i] = _kh_u2 + (_kh_u2-_kh_u1) * exp((-z[i]+zcntr)/(0.025*(xmax-xmin)));
           }
 
           // v
-          vel[i+2*npoints] = 0.5 * kh_u1 * sin(4.0 * M_PI * (xx[i] + 0.5 * (xmax-xmin))/(xmax-xmin))/ vel_unit;
+          vel[i+2*npoints] = 0.5 * _kh_u1 * sin(4.0 * M_PI * (x[i] + 0.5 * (xmax-xmin))/(xmax-xmin));
 
           eps[i] = pow(rho[i], eos_gamma - 1.0) / (eos_gamma - 1.0);
 
       } else if (CCTK_EQUALS (initial_hydro, "rt")) {
 
-          rho[i] = rho1 + (rho2 - rho1) * 0.5 * (1.0 + tanh((zz[i]-zcntr) / 0.9 * z_smooth));
+          rho[i] = _rho1 + (_rho2 - _rho1) * 0.5 * (1.0 + tanh((z[i]-zcntr) / 0.9 * z_smooth));
 
-          rho[i] *= exp(-g * yy[i] / (eos_gamma * RR * alp[i]*alp[i]));
+          rho[i] *= exp(-g * y[i] / (eos_gamma * RR * alp[i]*alp[i]));
 
           vel[i          ] = 0.0;
-          vel[i+2*npoints] = rt_amp * cos(2.0 * M_PI * xx[i] / (xmax-xmin)) * exp(-(zz[i]-zcntr)*(zz[i]-zcntr)/(rt_sigma*rt_sigma));
+          vel[i+2*npoints] = rt_amp * cos(2.0 * M_PI * x[i] / (xmax-xmin)) * exp(-(z[i]-zcntr)*(z[i]-zcntr)/(rt_sigma*rt_sigma));
 
           eps[i] = pow(rho[i], eos_gamma - 1.0) / (eos_gamma - 1.0);
 
       } else {
           CCTK_WARN (CCTK_WARN_ABORT, "incorrect initial_hydro");
       }
-
 
       // Check density not too small
       if (rho[i] < 1.e-20) {
